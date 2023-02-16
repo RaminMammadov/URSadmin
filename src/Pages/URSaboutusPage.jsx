52edabc74b21196f7950af95078dad55f071007d
Ramin Mammadov <ramin@MiWiFi-R3L-srv.foundin.az>
2023-02-17 02:08:33 +0400

    all data

--- src/Pages/URSaboutusPage.jsx
+++ src/Pages/URSaboutusPage.jsx
@@ -105,8 +105,6 @@ export default function URSaboutusPage() {
   // editorState.getCurrentContent().getPlainText();
   return (
     <div className={style.URSaboutusPage}>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
       {showSuccessAlert && <div className="alert alert-success" role="alert">
         Hakkımızda kısmı değiştirildi
       </div>}
